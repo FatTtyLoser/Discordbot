4e2063b4280a243f9f82b43dd19c5e94564bcd77
FatTtyLoser <fatttyloser@gmail.com>
2021-09-22 18:24:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
 with open('setting.json', mode='r', encoding = 'utf8') as jfile:
     jdata = json.load(jfile)
@@ -27,5 +28,17 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+@bot.command()
+async def 圖片(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file = pic)
+
+@bot.command()
+async def 超腥拳婦白冰冰EX2(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    # pic = discord.File(random_pic)
+    await ctx.send(random_pic)
 
 bot.run(jdata['TOKEN'])
