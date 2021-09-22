f7ac97bf9ed6b9afc0912724614f8b40e45ac35a
FatTtyLoser <fatttyloser@gmail.com>
2021-09-22 15:39:04 +0800

    Create bot.py
    add 2 events for discord_memer api.

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='[', intents = intents)
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(890082452792565801)
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(890082501383573555)
+    await channel.send(f'{member} leave')
+
+bot.run('ODg5NzIwODIzMTUyMjYzMjI5.YUlXFg.ay3GBVvCUNp2ZfLLpXUxcpG6_LY')
