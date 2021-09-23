ce051bbce6eb1a8cc3b4b76a0e985da62584b2d3
FatTtyLoser <fatttyloser@gmail.com>
2021-09-23 18:34:13 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,31 +16,6 @@ bot = commands.Bot(command_prefix='[', intents = intents)
 async def on_ready():
     print(">> Bot is online <<")
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(int(jdata['Welcome_channel']))
-    await channel.send(f'{member} join!')
-
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(int(jdata['Leave_channel']))
-    await channel.send(f'{member} leave')
-
-@bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f'cmds.{extension}')
-    await ctx.send(f'Loaded {extension} done.')
-
-@bot.command()
-async def unload(ctx, extension):
-    bot.unload_extension(f'cmds.{extension}')
-    await ctx.send(f'Un - Loaded {extension} done.')
-
-@bot.command()
-async def reload(ctx, extension):
-    bot.reload_extension(f'cmds.{extension}')
-    await ctx.send(f'Re - Loaded {extension} done.')
-
 for filename in os.listdir('./cmds'):
     if filename.endswith('.py'):
         bot.load_extension(F'cmds.{filename[:-3]}')
