e9cb3002f0cb1183223c5e1b644082c982721073
FatTtyLoser <fatttyloser@gmail.com>
2021-09-24 17:34:38 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -21,12 +21,21 @@ class Main(Cog_Extension):
         embed=discord.Embed(title="Fatiger_bot", url="https://www.youtube.com/watch?v=072tU1tamd0&ab_channel=Majima", description="just a bot", color=0xdea1a1, timestamp=datetime.datetime.utcnow())
         embed.set_author(name="Jefffery", url="https://www.youtube.com/watch?v=072tU1tamd0&ab_channel=Majima", icon_url="https://cdn.hk01.com/di/media/images/dw/20200831/377077163972759552.jpeg/V47dBJKbgpTNfW4-6pYOgzJ5Vl_PUCnkc3sM7XN7DO0?v=w1920")
         embed.set_thumbnail(url="https://cdn.hk01.com/di/media/images/dw/20200831/377077163972759552.jpeg/V47dBJKbgpTNfW4-6pYOgzJ5Vl_PUCnkc3sM7XN7DO0?v=w1920")
-        embed.add_field(name="最愛的偶像", value="白冰冰", inline=True)
-        embed.add_field(name="最愛的女人", value="黑冰冰", inline=False)
-        embed.add_field(name="最愛的狗", value="綠冰冰", inline=True)
-        embed.add_field(name="最愛的史黑", value="史冰冰", inline=False)
-        embed.set_footer(text="超腥拳婦吃我懶覺")
+        embed.add_field(name="1", value="4", inline=True)
+        embed.add_field(name="2", value="3", inline=False)
+        embed.add_field(name="3", value="2", inline=True)
+        embed.add_field(name="4", value="1", inline=False)
+        embed.set_footer(text="test_text")
         await ctx.send(embed=embed)
+
+    #刪除我所說的訊息、覆誦我所說的訊息。
+    @commands.command()
+    async def sayd(self, ctx, *,msg):
+        await ctx.message.delete()
+        await ctx.send(msg)
+    @commands.command()
+    async def purge(self, ctx, num : int):
+        await ctx.channel.purge(limit=num+1)
 
 def setup(bot):
     bot.add_cog(Main(bot))
